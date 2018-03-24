ee76896e0c8bc23b36b79f0b5fef435103cdb607
Sviatoslav Strus <svyatoslav@email.ua>
2018-03-24 16:47:59 +0200

    candy

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementsByClassName("header__wrapper--logo").addEventListener("click", giveAlert());
+    document.getElementsByClassName("");
+
+    function giveAlert() {
+        alert("Alert!!!");
+    }
+})
